c46d47df3d33f3329a130c37c19c93536d57edab
Leonardo <leonardo11emn@gmail.com>
2022-04-27 00:11:22 -0500

    Crear el navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Home } from "./componentes/Home";
+import { Navbar } from "./componentes/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Home/>
-    </div>
+      <Navbar/>
+    </>
   );
 }
 
